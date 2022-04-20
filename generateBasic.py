a1a7b939ffda3025abb95980ed6932373999e0d9
Brian Lawrence <Bobukanator@gmail.com>
2022-04-20 16:34:46 +0200

    Refactored and added support for ellipse, rectangle, and polygon

--- generateBasic.py
+++ generateBasic.py
@@ -1,4 +1,5 @@
 from PIL import Image, ImageDraw
+from enum import IntEnum
 import random
 
 # Global Variables
@@ -8,8 +9,18 @@ draw_padding = 15
 background_color = (255, 255, 255)
 imagename = "TestImage.png"
 directory = "art_generator/images/"
-# color palette
-main_color = (33, 255, 33)
+
+
+# INT Enum containing the different types of image.Draw techniques
+#
+#
+class DrawingTechniques(IntEnum):
+    LINE = 1
+    CHORD = 2
+    PIESLICE = 3
+    ELLIPSE = 4
+    RECTANGLE = 5
+    POLYGON = 6
 
 
 def createBlankCanvas():
@@ -36,55 +47,35 @@ def createRandomAngle():
     return random.randint(0, 360)
 
 
-def drawRandomPoints(image, pointCount):
+def DrawARandomTechnique(image, color):
     draw = ImageDraw.Draw(image)
-    pt_color = main_color
+    RandomTechnique = random.randint(1, len(DrawingTechniques))
 
-    for _ in range(pointCount):
-        draw.point(createRandomPoint(), fill=createRandomColor())
-
-
-def createRandomChordWithColor(image, locationPt, color):
-    draw = ImageDraw.Draw(image)
-    draw.chord((locationPt, createRandomPoint()), createRandomAngle(), createRandomAngle(),
-               fill=color, outline=(0, 0, 0), width=1)
-
-
-def createRandomPiesliceWithColor(image, locationPt, color):
-    draw = ImageDraw.Draw(image)
-    draw.pieslice((locationPt, createRandomPoint()), createRandomAngle(), createRandomAngle(),
-                  fill=color, outline=(0, 0, 0), width=1)
-
-
-def createRandomLines(image, lineCount):
-    draw = ImageDraw.Draw(image)
-
-    for _ in range(lineCount):
-        random_linestart = (
-            random.randint(0+draw_padding, max_px_canvas-draw_padding),
-            random.randint(0+draw_padding, max_px_canvas-draw_padding)
-        )
-        random_lineend = (
-            random.randint(0+draw_padding, max_px_canvas-draw_padding),
-            random.randint(0+draw_padding, max_px_canvas-draw_padding)
-        )
-
-        line_xy = (random_linestart, random_lineend)
-        draw.line(line_xy, fill=createRandomColor())
+    match RandomTechnique:
+        case DrawingTechniques.LINE:
+            draw.line((createRandomPoint(), createRandomPoint()), fill=color)
+        case DrawingTechniques.CHORD:
+            draw.chord((createRandomPoint(), createRandomPoint()), createRandomAngle(), createRandomAngle(),
+                       fill=color, outline=(0, 0, 0), width=1)
+        case DrawingTechniques.PIESLICE:
+            draw.pieslice((createRandomPoint(), createRandomPoint()), createRandomAngle(), createRandomAngle(),
+                          fill=color, outline=(0, 0, 0), width=1)
+        case DrawingTechniques.ELLIPSE:
+            draw.ellipse(
+                (createRandomPoint(), createRandomPoint()), fill=color, outline=(0, 0, 0), width=1)
+        case DrawingTechniques.RECTANGLE:
+            draw.rectangle(
+                (createRandomPoint(), createRandomPoint()), fill=color, outline=(0, 0, 0), width=1)
+        case DrawingTechniques.POLYGON:
+            draw.polygon((createRandomPoint(), createRandomPoint(),
+                         createRandomPoint()), fill=color, outline=(0, 0, 0), width=1)
 
 
 def generateBasic():
 
     image = createBlankCanvas()
-
-    createRandomLines(image, random.randint(0, 40))
-
-    for _ in range(random.randint(0, 40)):
-        createRandomChordWithColor(
-            image, createRandomPoint(), createRandomColor())
-    for _ in range(random.randint(0, 40)):
-        createRandomPiesliceWithColor(
-            image, createRandomPoint(), createRandomColor())
+    for _ in range(random.randint(0, 100)):
+        DrawARandomTechnique(image, createRandomColor())
 
     image.save(directory+imagename)
 
